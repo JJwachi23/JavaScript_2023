545b6d322f9a0748e2848ceb4f9133aae579ef84
JJ <admin@admins-iMac-3.local>
2024-01-15 15:33:15 +0700

    Challenge#1

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let fistName = "Wachirawit";
-console.log(fistName);
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJOHN = massJohn / (heightJohn * heightJohn);
+const markingHigherBMI = BMIMark > BMIJOHN;
+
+console.log(BMIMark, BMIJOHN, markingHigherBMI);
